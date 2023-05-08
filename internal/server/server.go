1a0c69212826255c66d5cdb85bb2f050b75af4cc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-08 13:36:07 -0600

    feat: added simple server implementation with `httprouter` library

---
+++ internal/server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/luisnquin/blind-creator-rest-api-test/internal/config"
+)
+
+type (
+	Server struct {
+		pathMethods map[string][]string
+		config      config.App
+		handlers    []handlerInfo
+	}
+
+	handlerInfo struct {
+		handler    Handler
+		method     string
+		path       string
+		withToken  bool
+		withApiKey bool
+	}
+
+	Handler func(w http.ResponseWriter, r *http.Request, p Params)
+)
+
+type Registerer interface {
+	RegisterHandler(path, method string, handler Handler, apiKey, token bool)
+}
+
+type Params = httprouter.Params
+
+func New(config config.App) Server {
+	return Server{
+		pathMethods: make(map[string][]string),
+		config:      config,
+	}
+}
+
+func (s Server) Start() error {
+	router := httprouter.New()
+
+	for _, endpoint := range s.handlers {
+		// handler := LogMiddleware(endpoint.handler)
+		handler := endpoint.handler
+
+		router.Handle(endpoint.method, endpoint.path, httprouter.Handle(handler))
+	}
+
+	if s.config.Server.Port[0] != ':' {
+		s.config.Server.Port = ":" + s.config.Server.Port
+	}
+
+	return http.ListenAndServe(s.config.Server.Port, router)
+}
