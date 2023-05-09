2707de57cb8cfb9a7ca7a3864f32528e0162c3e1
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-08 21:54:26 -0600

    feat: added function to create a new db connection with gorm

---
+++ internal/datalayer/datalayer.go
@@ -0,0 +1,38 @@
+package datalayer
+
+import (
+	"fmt"
+
+	"github.com/luisnquin/blind-creator-rest-api-test/internal/config"
+	"github.com/luisnquin/blind-creator-rest-api-test/internal/log"
+	"github.com/luisnquin/blind-creator-rest-api-test/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewForORM(config config.App) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Bogota",
+		config.Database.User(),
+		config.Database.Password(),
+		config.Database.Name(),
+		config.Database.Port(),
+	)
+
+	log.Trace().Msg("connecting to database...")
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		return nil, err
+	}
+
+	log.Trace().Msg("successfully connected...")
+
+	log.Trace().Msg("ensuring database tables...")
+
+	err = db.AutoMigrate(&models.Customer{}, &models.Order{}, &models.OrderItem{}, &models.Product{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
