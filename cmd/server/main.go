6986a56d6a5418d7e475798b02df8bbf09b08794
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-08 22:14:25 -0600

    fix(server): updated to the last draft

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"github.com/luisnquin/blind-creator-rest-api-test/internal/config"
+	"github.com/luisnquin/blind-creator-rest-api-test/internal/datalayer"
+	"github.com/luisnquin/blind-creator-rest-api-test/internal/log"
 	"github.com/luisnquin/blind-creator-rest-api-test/internal/server"
 )
 
 func main() {
-	appConfig := config.NewApp("")
+	appConfig := config.NewApp()
 
 	server := server.New(appConfig)
+
+	db, err := datalayer.NewForORM(appConfig)
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to create connection with db")
+	}
+
+	_ = db
 
 	if err := server.Start(); err != nil {
 		panic(err)
