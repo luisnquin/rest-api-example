730d76a564fc273081a4e7ad5f88cde5519b2b30
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-08 22:46:30 -0600

    chore(models): removed `price` field from `order item` gorm model

--- internal/models/order_item.go
+++ internal/models/order_item.go
@@ -1,11 +1,8 @@
 package models
 
-import "github.com/shopspring/decimal"
-
 type OrderItem struct {
-	OrderItemID string          `gorm:"primaryKey"`
-	OrderID     string          `gorm:"foreignKey"`
-	ProductID   string          `gorm:"foreignKey"`
-	Quantity    uint32          `gorm:"not null"`
-	Price       decimal.Decimal `gorm:"type:decimal(7,6);"`
+	OrderItemID string `gorm:"primaryKey"`
+	OrderID     string `gorm:"foreignKey"`
+	ProductID   string `gorm:"foreignKey"`
+	Quantity    uint32 `gorm:"not null"`
 }
