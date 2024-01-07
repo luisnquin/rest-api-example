d07dc890077cd50366974781d31d0da1eef2d389
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-07 16:59:57 -0500

    feat: endpoint to retrieve cities

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+
+	"github.com/luisnquin/server-example/internal/business/locations"
 	"github.com/luisnquin/server-example/internal/config"
+	"github.com/luisnquin/server-example/internal/database"
+	"github.com/luisnquin/server-example/internal/database/sqlc"
 	"github.com/luisnquin/server-example/internal/log"
 	"github.com/luisnquin/server-example/internal/server"
 )
@@ -11,10 +16,18 @@ func main() {
 
 	server := server.New(appConfig)
 
+	ctx := context.Background()
+
+	pool, err := database.NewConnectionPool(ctx, appConfig)
 	if err != nil {
 		log.Fatal().Err(err).Msg("unable to create connection with db")
 	}
 
+	defer pool.Close()
+
+	querier := sqlc.New(pool)
+
+	locations.NewManager(querier).RegisterHandlers(server)
 
 	if err := server.Start(); err != nil {
 		log.Fatal().Err(err).Msg("while the server was running...")
