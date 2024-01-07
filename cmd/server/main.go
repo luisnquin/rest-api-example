e6b31b0c15cd2ed7bd3de3e41253d606ec9314d4
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-07 16:52:57 -0500

    chore: excised old business logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"github.com/luisnquin/server-example/internal/business"
 	"github.com/luisnquin/server-example/internal/config"
-	"github.com/luisnquin/server-example/internal/datalayer"
 	"github.com/luisnquin/server-example/internal/log"
 	"github.com/luisnquin/server-example/internal/server"
 )
@@ -13,13 +11,10 @@ func main() {
 
 	server := server.New(appConfig)
 
-	db, err := datalayer.Connect(appConfig)
 	if err != nil {
 		log.Fatal().Err(err).Msg("unable to create connection with db")
 	}
 
-	businessMgr := business.NewManager(db)
-	businessMgr.RegisterHandlers(server)
 
 	if err := server.Start(); err != nil {
 		log.Fatal().Err(err).Msg("while the server was running...")
--- internal/models/customer.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Customer struct {
-	CustomerID  string `gorm:"primaryKey"`
-	FirstName   string `gorm:"not null"`
-	LastName    string `gorm:"not null"`
-	Email       string `gorm:"not null;unique"`
-	PhoneNumber string `gorm:"not null"`
-}
